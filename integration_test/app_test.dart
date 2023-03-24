178c1bf66e0562becf3f04b6d1a0453190d217f9
Nihal <nihalagazade@gmail.com>
2023-03-24 11:06:05 +0200

    typo in folder name

--- integration_test/app_test.dart
+++ integration_test/app_test.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:integration_test/integration_test.dart';
-import 'package:flutter_reproduction_app/main.dart';
+// import 'package:flutter_reproduction_app/main.dart';
 // import 'dart:io';
 
 void main() {
